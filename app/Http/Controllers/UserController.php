357727f4bdbca6ff385e6231328b831e70365aaa
=Hilmy1789 <=helmyzakim@gmail.com>
2024-09-20 08:38:34 +0700

    Pertemuan4-Praktikum2.4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
         // ];
         // UserModel::where('username', 'customer-1')->update($data);
 
-        // // coba akses model  UserModel
+        // coba akses model  UserModel
         // $user = UserModel::all(); //ambil semua data dari tabel m_user
         // return view('user', ['data' => $user]);
         //P4Praktikum1
@@ -38,7 +38,8 @@ class UserController extends Controller
         // $user = UserModel::findOrFail(1);
         // return view('user', ['data' => $user]);
         
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
-        return view('user', ['data' => $user]);
+        // $user = UserModel::where('level_id', 2)->count();
+        // // dd($user);
+        // return view('user', ['data' => $user]);
     }
 }
