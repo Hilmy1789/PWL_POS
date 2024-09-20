28cd0092876cc162a4285116362b8ffe0c52909b
=Hilmy1789 <=helmyzakim@gmail.com>
2024-09-20 08:04:09 +0700

    Pertemuan4-Praktikum1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,14 +9,23 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        //tambah data user dengan Eloquent Model
-        $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1')->update($data);
+        // //tambah data user dengan Eloquent Model
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama',
+        // ];
+        // UserModel::where('username', 'customer-1')->update($data);
 
-        // coba akses model  UserModel
-        $user = UserModel::all(); //ambil semua data dari tabel m_user
-        return view('user', ['data' => $user]);
+        // // coba akses model  UserModel
+        // $user = UserModel::all(); //ambil semua data dari tabel m_user
+        // return view('user', ['data' => $user]);
+        //P4Praktikum1
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'name' => 'Manager 3',
+            'password' => Hash::make('12345'),
+        ];
+        UserModel::create($data);
+
     }
 }
