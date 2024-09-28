a9db45bc06d0fda117cd7c8c18cb0bc2b025723d
=Hilmy1789 <=helmyzakim@gmail.com>
2024-09-28 23:20:44 +0700

    Pertemuan5-Praktikum4&Tugas

--- app/Models/LevelModel.php
+++ app/Models/LevelModel.php
@@ -4,17 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class LevelModel extends Model
 {
     use HasFactory;
-    
-    protected $table = 'm_level'; // Mendefinisikan nama tabel
-    protected $primaryKey = 'level_id'; // Mendefinisikan primary key
 
-    // Relasi ke UserModel (hasMany)
-    public function users(): HasMany {
-        return $this->hasMany(UserModel::class, 'level_id', 'level_id');
+    protected $table = 'm_level'; // Mendefinisikan nama tabel yang digunakan oleh model ini
+    protected $primaryKey = 'level_id'; // Mendefinisikan primary key dari tabel yang digunakan
+
+    protected $fillable = ['level_kode','level_nama'];
+
+    public function user():HasMany
+    {
+        return $this->hasMany(UserModel::class, 'user_id', 'user_id');
     }
 }
