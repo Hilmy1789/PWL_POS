dcefa924f08e4df056c2c78693da84fddf3f2c0c
=Hilmy1789 <=helmyzakim@gmail.com>
2024-10-18 08:42:57 +0700

    Pertemuan8-praktikum2

--- routes/web.php
+++ routes/web.php
@@ -133,6 +133,7 @@ Route::middleware(['auth'])->group(function(){//artinya semua route di dalam fro
             Route::delete('/barang/{id}', [BarangController::class, 'destroy']); // menghapus data barang
             Route::get('/barang/import',[BarangController::class, 'import']);//ajax form upload excel
             Route::post('/barang/import_ajax',[BarangController::class,'import_ajax']);//ajax import excel
+            Route::get('/barang/export_excel',[BarangController::class,'export_ajax']);//ajax export excel
     });    
 });
 
