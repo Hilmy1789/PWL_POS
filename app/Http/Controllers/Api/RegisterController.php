1ee380cb110c36d6665e11fa682419c5440a3d69
=Hilmy1789 <=helmyzakim@gmail.com>
2024-11-07 23:12:30 +0700

    Jobsheet11-Praktikum&Tugas

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -11,36 +11,40 @@ class RegisterController extends Controller
 {
     public function __invoke(Request $request)
     {
+        // set validation
         $validator = Validator::make($request->all(), [
-            'username'  => 'required',
-            'nama'      => 'required',
-            'password'  => 'required|min:5|confirmed',
-            'level_id'  => 'required',
-            'foto'      => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:4096',
+            'username' => 'required',
+            'nama' => 'required',
+            'password' => 'required|min:5|confirmed',
+            'level_id' => 'required',
+            'profile_image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
         ]);
 
+        // if validation fails
         if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
         }
 
-        $foto  = $request->foto;
+        // create user  
         $user = UserModel::create([
-            'username'  => $request->username,
-            'nama'      => $request->nama,
-            'password'  => bcrypt($request->password),
-            'level_id'  => $request->level_id,
-            'foto'      => $foto->hashName(),
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => bcrypt($request->password),
+            'level_id' => $request->level_id,
+            'profile_image' => $request->profile_image->hashName()
         ]);
 
+        // return response JSON if user is created
         if ($user) {
             return response()->json([
-                'success'   => true,
-                'user'      => $user,
-            ],201);
+                'success' => true,
+                'user' => $user,
+            ], 201);
         }
 
+        // return JSON if process insert failed
         return response()->json([
-            'success'   => false,
-        ],409);
+            'success' => false,
+        ], 409);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,10 @@
 <?php
 
-use App\Http\Controllers\Api\LevelController;
-use App\Http\Controllers\Api\UserController;
-use App\Http\Controllers\Api\KategoriController;
 use App\Http\Controllers\Api\BarangController;
-use App\Http\Controllers\Api\SupplierController;
-use App\Http\Controllers\Api\StokController;
+use App\Http\Controllers\Api\KategoriController;
+use App\Http\Controllers\Api\LevelController;
+use App\Http\Controllers\Api\PenjualanController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,11 +20,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/register1', App\Http\Controllers\Api\RegisterController::class)->name('register1');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
-Route::middleware('auth:api')->get('/user', function (Request $request){
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
 
 Route::get('levels', [LevelController::class, 'index']);
 Route::post('levels', [LevelController::class, 'store']);
@@ -51,14 +51,8 @@ Route::get('barang/{barang}', [BarangController::class, 'show']);
 Route::put('barang/{barang}', [BarangController::class, 'update']);
 Route::delete('barang/{barang}', [BarangController::class, 'destroy']);
 
-Route::get('supplier', [SupplierController::class, 'index']);
-Route::post('supplier', [SupplierController::class, 'store']);
-Route::get('supplier/{supplier}', [SupplierController::class, 'show']);
-Route::put('supplier/{supplier}', [SupplierController::class, 'update']);
-Route::delete('supplier/{supplier}', [SupplierController::class, 'destroy']);
-
-Route::get('stok', [StokController::class, 'index']);
-Route::post('stok', [StokController::class, 'store']);
-Route::get('stok/{stok}', [StokController::class, 'show']);
-Route::put('stok/{stok}', [StokController::class, 'update']);
-Route::delete('stok/{stok}', [StokController::class, 'destroy']);
+Route::get('penjualan', [PenjualanController::class, 'index']);
+Route::post('penjualan', [PenjualanController::class, 'store']);
+Route::get('penjualan/{penjualan}', [PenjualanController::class, 'show']);
+Route::put('penjualan/{penjualan}', [PenjualanController::class, 'update']);
+Route::delete('penjualan/{penjualan}', [PenjualanController::class, 'destroy']);
